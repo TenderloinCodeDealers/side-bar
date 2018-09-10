931168dafff7398054352a1349dde184b4da6f71
Huan L <huanl@Huans-MacBook-Pro.local>
2018-09-10 14:18:10 -0700

    Fixed a small bug in server's index.js file

--- server/index.js
+++ server/index.js
@@ -8,12 +8,11 @@ app.get('/:id/api/products', (req, res) => {
 
   controller.getProduct(productId, (err, data) => {
     if(err) {
-      // res.status(503).send(err);
-      // console.log('Error, in getting data from DB')
       res.status(500).send('Error, in getting data from DB');
     } else {
-      res.status(200).send('Success, in getting data from DB');
       res.send(data);
+      // Note: the latter does not work, because res cannot be sent twice
+      res.status(200);  //res.status(200).send('Success, in getting data from DB');
     }
   })
 });
