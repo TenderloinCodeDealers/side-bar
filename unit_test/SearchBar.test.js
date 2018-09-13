8cc0b60736fe8e6ed40c1acf3ac2d4f3b520693a
Huan L <huanl@Huans-MacBook-Pro.local>
2018-09-13 16:51:59 -0700

    Incoporated Vik's code review comments on unit tests; used Enzyme's method to check only part of a paragraph

--- unit_test/SearchBar.test.js
+++ unit_test/SearchBar.test.js
@@ -13,13 +13,18 @@ it('renders without crashing', () => {
 
 
 // Test area 2: test if the form works, for searching for product ID
+// Improvement (per Vik): use a variable to store shallow-rendered React component
 describe('Search Form', () => {
+  const searchBar = shallow(<SearchBar />); // Improvement
+
   it('should render form without throwing an error', () => {
-    expect(shallow(<SearchBar />).find('form').exists()).toBe(true);
+    // expect(shallow(<SearchBar />).find('form').exists()).toBe(true);
+    expect(searchBar.find('form').exists()).toBe(true);
   });
   
   it('form should render product ID input', () => {
-    expect(shallow(<SearchBar />).find('#productID').exists()).toBe(true);
+    // expect(shallow(<SearchBar />).find('#productID').exists()).toBe(true);
+    expect(searchBar.find('#productID').exists()).toBe(true);
   })
 });
 
--- unit_test/SideBar.test.js
+++ unit_test/SideBar.test.js
@@ -7,14 +7,20 @@ import { shallow } from 'enzyme'; // Needs the adaptor from Enzyme
 import SideBar from '../client/src/components/SideBar.jsx';
 import 'jest-enzyme';
 
+
 it('renders without crashing', () => {
   shallow(<SideBar />);
 });
 
-// [Todo] Figure out how to test for part of <p></p> (e.g. just 'Expiration'), in addition to the entire <p></p>
-// it('renders the words, as selected', () => {
-//   const wrapper = shallow(<SideBar />);
-//   const words = <p>Expiration</p>
-//   expect(wrapper).toContainReact(words); // Syntax with 'jest-enzyme'
-// });
+
+// Note: look to match only part of the paragraph
+// Found syntax at doc, https://github.com/FormidableLabs/enzyme-matchers
+it('renders the words, as selected', () => {
+  const wrapper = shallow(<SideBar />);
+  expect(wrapper).toIncludeText('Expiration');
+
+  // Note: look to match the entire paragraph
+  // const words = Expiration;
+  // expect(wrapper).toContainReact(words); // Syntax with 'jest-enzyme'
+});
 
