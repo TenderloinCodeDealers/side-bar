561960d688c2b3ae9dfeec727241069f48e3ce5b
Huan L <huanl@huans-mbp.lan>
2018-09-15 18:41:34 -0700

    Adjusted client's App.jsx and server's index.js, that html page only loads data when an ID is in the url

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,97 +1,107 @@
 import React from 'react';
-import axios from 'axios'; // import $ from 'jquery';
+
+// Note: another option for HTTP method is to use ES6's built-in function, fetch()
+import axios from 'axios';
+// import $ from 'jquery';
 
 import SearchBar from './SearchBar.jsx';
 import PhotoCarousel from './PhotoCarousel.jsx';
 import SideBar from './SideBar.jsx';
 
-
 class App extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			productName: '',
-			category: '',
-			expirationTime: '',
+  constructor(props) {
+    super(props);
+    this.state = {
+      productName: '',
+      category: '',
+      expirationTime: '',
 
-			option1Price: '',
-			option1Warranty: '',
-			option2Price: '',
-			option2Warranty: ''
-		};
-	}
+      option1Price: '',
+      option1Warranty: '',
+      option2Price: '',
+      option2Warranty: ''
+    };
+  }
 
-	// About method: get products data from server
-	getProducts(id) {
-		const context = this;
+  // Note: added 'this.getProducts(id)' to component lifecycle, that 'http://localhost:3004/' only displays the page when there is a 'id' at the end
+  componentDidMount() {
+    const url = window.location.href.replace(/^(?:\/\/|[^/]+)*\//, '');
+    const id = url.substring(0, url.indexOf('/'));
+    this.getProducts(id);
+  }
 
-		// Note: use axios, for HTTP request, get
-		axios.get(`/${id}/api/products`) // axios.get('/:id/api/products')
-			.then(function (response) {
-				// console.log(JSON.stringify(response)); // For debugging
-				context.setState({
-					// product: response.data[0], // Product's 'Option 1' row
-					productName: response.data[0].product_name,
-					category: response.data[0].category,
-					expirationTime: response.data[0].expiration_time,
+  // About method: get products data from server
+  getProducts(id) {
+    const context = this;
 
-					option1Price: response.data[0].product_price,
-					option1Warranty: response.data[0].warranty_cost,
-					option2Price: response.data[1].product_price,
-					option2Warranty: response.data[1].warranty_cost
-				});
-			})
-			.catch(function (error) {
-				console.log(error);
-			});
-	}
+    // Note: use axios, for HTTP request, get
+    axios
+      .get(`/${id}/api/products`) // ES6's template literal
+      .then(function(response) {
+        // console.log(JSON.stringify(response)); // For debugging
+        context.setState({
+          // product: response.data[0], // Product's 'Option 1' row
+          productName: response.data[0].product_name,
+          category: response.data[0].category,
+          expirationTime: response.data[0].expiration_time,
 
-	// Search for deal, by product ID
-	handleSearch(value) {
-		this.getProducts(value);
-	}
+          option1Price: response.data[0].product_price,
+          option1Warranty: response.data[0].warranty_cost,
+          option2Price: response.data[1].product_price,
+          option2Warranty: response.data[1].warranty_cost
+        });
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
 
-	render() {
-		return (
-			// Note: need to use "wrapper" as the className
-			<div className="wrapper">
-				<div className="rowDivider">
-					<h3>Welcome to Vourcher!</h3>
-				</div>
-				<div className="rowDivider">
-					<br></br>
-				</div>
+  // Search for deal, by product
+  // Note: search by ID is optional now, because 'getProducts(id)' is also added to component lifecycle
+  handleSearch(value) {
+    this.getProducts(value);
+  }
 
-				<div className="rowDivider">
-					{/* Note: do not use the first line below (from React's tutorial) */}
-					{/* <SearchBar handleSearch={() => this.handleSearch.bind(this)} /> */}
-					<SearchBar handleSearch={this.handleSearch.bind(this)} />
-					<br></br><br></br>
-				</div>
+  render() {
+    return (
+      // Note: need to use "wrapper" as the className
+      <div className="wrapper">
+        <div className="rowDivider">
+          <h3>Welcome to Vourcher!</h3>
+        </div>
+        <div className="rowDivider">
+          <br />
+        </div>
 
-				<div className="rowDivider">
-					<br></br>
-				</div>
+        <div className="rowDivider">
+          {/* Note: do not use the first line below (from React's tutorial) */}
+          {/* <SearchBar handleSearch={() => this.handleSearch.bind(this)} /> */}
+          <SearchBar handleSearch={this.handleSearch.bind(this)} />
+          <br />
+          <br />
+        </div>
 
-				<div className="columnDivider">
-					<PhotoCarousel productName={this.state.productName} />
-				</div>
+        <div className="rowDivider">
+          <br />
+        </div>
 
-				<div>
-					<SideBar
-						category={this.state.category}
-						expirationTime={this.state.expirationTime}
+        <div className="columnDivider">
+          <PhotoCarousel productName={this.state.productName} />
+        </div>
 
-						option1Price={this.state.option1Price}
-						option1Warranty={this.state.option1Warranty}
-						option2Price={this.state.option2Price}
-						option2Warranty={this.state.option2Warranty}
-					/>
-				</div>
-			</div>
-		);
-	}
+        <div>
+          <SideBar
+            category={this.state.category}
+            expirationTime={this.state.expirationTime}
+            option1Price={this.state.option1Price}
+            option1Warranty={this.state.option1Warranty}
+            option2Price={this.state.option2Price}
+            option2Warranty={this.state.option2Warranty}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
-
--- server/index.js
+++ server/index.js
@@ -2,24 +2,26 @@ const express = require('express');
 const app = express();
 const controller = require('../database/index.js');
 
-app.use(express.static(__dirname + '/../client/dist'));
-
+// Note: added '/:id', so that urls (e.g. http://localhost:3004/55/) work
+app.use('/:id', express.static(__dirname + '/../client/dist'));
+// app.use(express.static(__dirname + '/../client/dist'));
 
 app.get('/:id/api/products', (req, res) => {
-  let productId = req.params.id;  //var productId = req.params.id;
+  let productId = req.params.id; //var productId = req.params.id;
   console.log('productID: ' + productId);
 
   controller.getProduct(productId, (err, data) => {
-    if(err) {
+    if (err) {
       res.status(500).send('Error, in getting data from DB');
     } else {
       res.send(data);
       // Note: the latter does not work, because res cannot be sent twice
-      res.status(200);  //res.status(200).send('Success, in getting data from DB');
+      res.status(200); //res.status(200).send('Success, in getting data from DB');
     }
-  })
+  });
 });
 
 const PORT = process.env.PORT || 3004;
-app.listen(PORT, () => {console.log(`Listening on port ${PORT}.`)});
-
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}.`);
+});
